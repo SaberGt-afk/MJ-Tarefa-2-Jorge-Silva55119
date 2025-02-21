3eeee2a92e4acdda35f3e7c12f6b4050c139576f
SaberGt-afk <gtsaber0000@gmail.com>
2025-02-21 20:39:16 +0000

    Make your game frame rate independent

--- Assets/Scripts/PlayerControlle.cs
+++ Assets/Scripts/PlayerControlle.cs
@@ -10,15 +10,15 @@ public class PlayerControllerTutorialUpdates : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MoveAction.Enable(); // Habilita a ação de input MoveAction
+      MoveAction.Enable(); // Habilita a ação de input MoveAction
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
-        transform.position = position;
+      Vector2 move = MoveAction.ReadValue<Vector2>();
+      Debug.Log(move);
+      Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+      transform.position = position;
     }
 }
