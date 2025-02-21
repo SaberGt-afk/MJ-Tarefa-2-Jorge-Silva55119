ac6a0ace75bfb08a8e1659a2e32055799ea8c44b
SaberGt-afk <gtsaber0000@gmail.com>
2025-02-21 15:15:19 +0000

    Test your changes

--- Assets/Scripts/PlayerControlle.cs
+++ Assets/Scripts/PlayerControlle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControlle : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,8 +13,8 @@ public class PlayerControlle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        Vector3 position = transform.position;
+        position.x += 0.1f; 
         transform.position = position;
     }
 }
