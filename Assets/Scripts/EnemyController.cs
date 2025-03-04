70a622ad3780b01ebe8f38e91bdcc852dc58df93
SaberGt-afk <gtsaber0000@gmail.com>
2025-03-04 20:28:05 +0000

    Configure projectiles to affect the enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,71 +4,96 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Public variables
-    public float speed = 1.0f;
-    public bool vertical;
-    public float changeTime = 3.0f;
+   // Public variables
+   public float speed;
+   public bool vertical;
+   public float changeTime = 3.0f;
+  
+   // Private variables
+   Rigidbody2D rigidbody2d;
+   Animator animator;
+   float timer;
+   int direction = 1;
+   bool broken = true;
 
-    // Private variables
-    private Rigidbody2D rigidbody2d;
-    private float timer;
-    private int direction = 1;
 
-    // Animator component reference
-    private Animator animator;
+   // Start is called before the first frame update
+   void Start()
+   {
+       rigidbody2d = GetComponent<Rigidbody2D>();
+       animator = GetComponent<Animator>();
+       timer = changeTime;
+      
+   }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();  // Retrieve Animator component
-        timer = changeTime;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // Update timer
+ // Update is called every frame
+  void Update()
+  {
         timer -= Time.deltaTime;
 
-        // Change direction when timer reaches zero
-        if (timer <= 0)
+
+        if (timer < 0)
         {
             direction = -direction;
             timer = changeTime;
         }
-    }
+  }
 
-    // FixedUpdate is used for physics calculations
-    void FixedUpdate()
+
+  // FixedUpdate has the same call rate as the physics system
+  void FixedUpdate()
+  {
+      if(!broken)
+      {
+           return;
+      }
+     
+       Vector2 position = rigidbody2d.position;
+     
+       if (vertical)
+       {
+           position.y = position.y + speed * direction * Time.deltaTime;
+           animator.SetFloat("Move X", 0);
+           animator.SetFloat("Move Y", direction);
+       }
+       else
+       {
+           position.x = position.x + speed * direction * Time.deltaTime;
+           animator.SetFloat("Move X", direction);
+           animator.SetFloat("Move Y", 0);
+       }
+
+
+       rigidbody2d.MovePosition(position);
+  }
+
+
+   void OnTriggerEnter2D(Collider2D other)
+   {
+       PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+
+       if (player != null)
+       {
+           player.ChangeHealth(-1);
+       }
+   }
+
+
+ void OnCollisionEnter2D(Collision2D collision)
     {
-        Vector2 position = rigidbody2d.position;
-        
-        if (vertical)
-        {
-            position.y += speed * direction * Time.deltaTime;
-            // Update animator for vertical movement
-            animator.SetFloat("Move X", 0);
-            animator.SetFloat("Move Y", direction); // "direction" controls up/down movement
-        }
-        else
-        {
-            position.x += speed * direction * Time.deltaTime;
-            // Update animator for horizontal movement
-            animator.SetFloat("Move X", direction); // "direction" controls left/right movement
-            animator.SetFloat("Move Y", 0);
-        }
-
-        rigidbody2d.MovePosition(position);
+        Destroy(gameObject);
     }
 
-    // Handle collision with the player
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        PlayerController player = other.gameObject.GetComponent<PlayerController>();
-        if (player != null)
-        {
-            player.ChangeHealth(-1);
-        }
-    }
+
+
+   public void Fix()
+   {
+       broken = false;
+       GetComponent<Rigidbody2D>().simulated = false;
+       animator.SetTrigger("Fixed");
+   }
+
+
 }
